8d44c1638f206ec076bad2f0242edf8d21a57c66
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-03-31 16:46:02 -0500

    add necessary files: q qw
    :wq

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+  <?php
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+      get_template_part( 'template-parts/content', get_post_format() );
+    endwhile;
+  else :
+    get_template_part( 'template-parts/content', 'none' );
+  endif;
+  ?>
+</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
