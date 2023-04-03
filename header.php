9c241a0e7ce6f701a399fd20ed21d785dd17c95d
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-03 03:49:25 -0500

    update the header

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <header>
+  <h1>CodeCrafters</h1>
+  <button id="sidebar-toggle" class="sidebar-toggle">Toggle Sidebar</button>
+
+  
+  </header>
