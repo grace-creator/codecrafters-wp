96d0226df172496aeae594d3035d934dc35427ce
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-03 03:45:56 -0500

    add footer details

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>© <?php echo date('Y'); ?> CodeCrafters. All rights reserved. Licensed under the GNU General Public License v2 or later.</p>
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
