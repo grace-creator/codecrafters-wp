25c71a5c71550fb6c981c4f9b7e5881bbdfd4c18
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-06 03:13:23 -0500

    add JavaScript for image

--- footer.php
+++ footer.php
@@ -1,6 +1,20 @@
 <footer>
     <p>© <?php echo date('Y'); ?> CodeCrafters. All rights reserved. Licensed under the GNU General Public License v2 or later.</p>
   </footer>
+  <script>
+  window.addEventListener("scroll", function() {
+  const ghostImage = document.getElementById("ghost-image");
+  const maxScroll = document.body.scrollHeight - window.innerHeight;
+  const scrollPercentage = window.scrollY / maxScroll;
+  const maxOpacity = 1;
+
+  // Move the image down and decrease its opacity.
+  ghostImage.style.transform = `translateY(${scrollPercentage * 100}%)`;
+  ghostImage.style.opacity = maxOpacity - scrollPercentage;
+});
+
+</script>
+
   <?php wp_footer(); ?>
 </body>
 </html>
