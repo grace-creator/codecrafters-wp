e3292906145fcf45568a3efa1e0d9d23e545acf6
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-06 03:18:25 -0500

    add hamburger icon to sidebar

--- header.php
+++ header.php
@@ -3,12 +3,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
   <header>
   <h1>CODECRAFTERS</h1>
-  <button id="sidebar-toggle" class="sidebar-toggle">Home</button>
+  <button id="sidebar-toggle" class="sidebar-toggle">
+    <span class="hamburger-icon"></span>
 
-  
+  </button>
+
   </header>
