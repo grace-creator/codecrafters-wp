ddc2b5a822a6848cf3e1d404f81fc7b15aae89ab
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-06 03:20:41 -0500

    add images

--- index.php
+++ index.php
@@ -6,30 +6,34 @@
       <h2>Welcome to my Minimalist Theme</h2>
       <p>This minimalist theme is designed to make your content shine. Its simplicity and clean design ensure that your users have an enjoyable experience navigating your website.</p>
     </div>
-    <div class="block-1-image">
-      <!-- Add your image here -->
+    <div 
+    class="block-1-image" 
+    id="ghost-image"
+    style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/ghost2.png');">
     </div>
   </section>
   
   <section class="block block-2">
     <div class="block-2-text">
-      <h3>Feature 1</h3>
-      <p>Describe the feature here.</p>
+      <h3>Responsive Design</h3>
+      <p>A responsive design ensures that the theme adapts well to different screen sizes and devices, providing a seamless user experience across different devices.</p>
     </div>
     <div class="block-2-text">
-      <h3>Feature 2</h3>
-      <p>Describe the feature here.</p>
+      <h3>Customization Options</h3>
+      <p>Users appreciate having control over various design elements, such as colors, fonts, layouts, and backgrounds.</p>
     </div>
     <div class="block-2-text">
-      <h3>Feature 3</h3>
-      <p>Describe the feature here.</p>
+      <h3>SEO Optimization</h3>
+      <p>The theme includes the use of headings, clean and lightweight code, and fast loading times to improve search engine rankings.</p>
     </div>
   </section>
   
   <section class="block block-3">
-    <div class="block-3-image">
-      <!-- Add your large image here -->
+    <div class="block-3-image"
+    style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/closed-e.png');">
     </div>
+    </div>
+  
   </section>
 
 
