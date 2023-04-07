e98b84d2a7d6517664445096be8d98afc10d3e22
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-06 19:26:06 -0500

    add hamburger style JavaScript

--- sidebar.js
+++ sidebar.js
@@ -1,10 +1,6 @@
-
-
 jQuery(document).ready(function ($) {
-  
-  
   $('#sidebar-toggle').on('click', function () {
-    
     $('body').toggleClass('sidebar-visible');
+    $('#sidebar-toggle').toggleClass('active');
   });
 });
