e42e1e8ae07463dbd74530580c9053c97e563e94
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-10 13:10:12 -0500

    add menu on top left of page

--- header.php
+++ header.php
@@ -4,17 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
   <header>
-  <h1>CODECRAFTERS</h1>
-  <button id="sidebar-toggle" class="sidebar-toggle">
-  <span></span>
-  <span></span>
-  <span></span>
-  <span></span>
-</button>
-
+    <div class="header-content">
+      <h1>CODECRAFTERS</h1>
+      <nav class="main-navigation">
+        <?php
+        wp_nav_menu( array(
+          'theme_location' => 'primary-menu',
+          'container'      => false,
+          'fallback_cb'    => false,
+        ) );
+        ?>
+      </nav>
+    </div>
   </header>
