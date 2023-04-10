b84c12251993ba3474e4bc822716da69613f2306
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-10 13:05:45 -0500

    remove sidebar.js

--- sidebar.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function ($) {
-  $('#sidebar-toggle').on('click', function () {
-    $('body').toggleClass('sidebar-visible');
-    $('#sidebar-toggle').toggleClass('active');
-  });
-});
