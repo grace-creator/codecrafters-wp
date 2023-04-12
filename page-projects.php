29c9979c9dffed120d9da1c7af96d34b888328d7
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-12 04:56:32 -0500

    add image and style to make the dog peek-a-boo

---
+++ page-projects.php
@@ -0,0 +1,23 @@
+<?php
+/* Template Name: Projects */
+
+get_header();
+?>
+
+<main class="projects-container">
+    
+<div class="project">
+  <h3 class="project-title">
+    Project Title 1
+  </h3>
+  <p class="project-description">A short description of the project goes here.</p>
+  <img src="<?php echo get_template_directory_uri(); ?>/images/golden.png" alt="Golden Retriever" class="project-image" />
+</div>
+
+<!-- Add more projects as needed -->
+
+
+
+</main>
+
+<?php get_footer(); ?>
