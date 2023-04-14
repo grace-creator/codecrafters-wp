c9ac49f7a7302eb0b3980dfe7418040375139001
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-04-13 22:23:08 -0500

    add more project examples

--- page-projects.php
+++ page-projects.php
@@ -13,11 +13,24 @@ get_header();
   <p class="project-description">A short description of the project goes here.</p>
   <img src="<?php echo get_template_directory_uri(); ?>/images/golden.png" alt="Golden Retriever" class="project-image" />
 </div>
+ 
+<div class="project">
+  <h3 class="project-title">
+    Project Title 2
+  </h3>
+  <p class="project-description">A short description of the project goes here.</p>
+  <img src="<?php echo get_template_directory_uri(); ?>/images/golden.png" alt="Golden Retriever" class="project-image" />
+</div>
+
+<div class="project">
+  <h3 class="project-title">
+    Project Title 3
+  </h3>
+  <p class="project-description">A short description of the project goes here.</p>
+  <img src="<?php echo get_template_directory_uri(); ?>/images/golden.png" alt="Golden Retriever" class="project-image" />
+</div>
 
 <!-- Add more projects as needed -->
-
-
-
 </main>
 
 <?php get_footer(); ?>
